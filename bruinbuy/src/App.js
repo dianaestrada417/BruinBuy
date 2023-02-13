bd130b4716745201d438871ae29c93048d3be4eb
Emanuel-Zavalza <emanuelzavalza@gmail.com>
2023-02-12 16:21:16 -0800

    Add logo to center of screen

--- bruinbuy/src/App.js
+++ bruinbuy/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import logo from './bruinBuyLogoWhite.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" height={250} width={200} />
         <h1>BruinBuy</h1>
       </header>
     </div>
