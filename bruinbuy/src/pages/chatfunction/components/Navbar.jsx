f9fcdfc22be7727fd572c9fe4c7b5a25a2ceb324
Peyton Ables <peytonables1@gmail.com>
2023-03-09 12:52:22 -0800

    Adding sidebar and navbar to chat
    There is a sidebar for the chat that will eventually show user chats. The sidebar now displays the name of the current user as well.

---
+++ bruinbuy/src/pages/chatfunction/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useState, useEffect } from "react";
+import { UserContext } from '../../../contexts/UserContext';
+import {db} from '../../../firebase-config';
+import {getDoc, doc} from "firebase/firestore"; 
+
+
+const Navbar = () => {
+
+    const {User, getUser} = useContext(UserContext)
+    const [userFullname, setUserFullname] = useState('');
+
+    useEffect(() => {
+        const getUserName = async () => {
+            const userRef = doc(db, "signups", User)
+            const docSnap = await getDoc(userRef);
+            setUserFullname(docSnap.data().fullName)
+            console.log(docSnap.data().fullName)
+            console.log(userFullname)
+        };
+    
+        getUserName();
+      }, []);
+
+   
+
+    return (
+      <div className='navbar'>
+        <span className="logo">BruinBuy Chat</span>
+        <div className="user">
+          <span>{userFullname}</span>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Navbar
--- bruinbuy/src/pages/chatfunction/components/Sidebar.jsx
+++ bruinbuy/src/pages/chatfunction/components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from './Navbar'
 
 const Sidebar = () => {
     return(
-        <div className="sidebar">SideBar
+        <div className="sidebar">
+            <Navbar>
+
+            </Navbar>
+
         </div>
     )
 }
