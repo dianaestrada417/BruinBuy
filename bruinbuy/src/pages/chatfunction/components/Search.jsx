905db6793b9ca2d943bd6e58b601d685cd436a82
Peyton Ables <peytonables1@gmail.com>
2023-03-11 02:10:25 -0700

    Added full implementation of search
    Search in the chat function is now fully implemented so that a user can search another user. Once they search this user and click their name, it will save a chat between the two users in firebase.

--- bruinbuy/src/pages/chatfunction/components/Search.jsx
+++ bruinbuy/src/pages/chatfunction/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { collection, query, where, getDocs, doc} from "firebase/firestore"
+import { collection, query, where, getDocs, doc, setDoc, updateDoc, serverTimestamp, getDoc} from "firebase/firestore"
 import { UserContext } from '../../../contexts/UserContext';
 import {db} from '../../../firebase-config';
 
@@ -9,6 +9,7 @@ const Search = () => {
     const [userFullname, setUserFullname] = useState('');
     const[fullname, setFullname] = useState("")
     const[messageUser, setMessageUser] = useState(null)
+    const[messageUserId, setMessageUserId] = useState(null)
     const[err, setErr] = useState(false)
 
     const handleSearch = async() => {
@@ -21,6 +22,7 @@ const Search = () => {
                 const querySnapshot = await getDocs(q)
                 querySnapshot.forEach((doc) => {
                     setMessageUser(doc.data())
+                    setMessageUserId(doc.id)
                 });
                 setErr("")
             }catch(err){
@@ -33,33 +35,48 @@ const Search = () => {
         e.code === "Enter" && handleSearch()
     }
 
-    /*const[fullname, setFullname] = useState("")
-    const[user, setUser] = useState(null)
-    const[err, setErr] = useState(false)
+    const handleSelect = async() => {
+        const getUserName = async () => {
+            const userRef = doc(db, "signups", User)
+            const docSnap = await getDoc(userRef);
+            setUserFullname(docSnap.data().fullName)
+        };
+        
+        getUserName();
 
-    const handleSearch = async () => {
-        const q = query(
-            collection(db, 'signups'), 
-            where('fullName', "==", fullname)
-            );
+        const combineId = User > messageUserId 
+        ? User + messageUserId: 
+        User + messageUserId
+        
+        try {
+            const res = await getDoc(doc(db, "chats", combineId))
 
-            try{
-                const querySnapshot = await getDocs(q)
-                querySnapshot.forEach((doc) => {
-                    setUser(doc.data())
+            if(!res.exists()) {
+                await setDoc(doc(db,"chats", combineId),{messages:[]})
+
+                await updateDoc(doc(db, "userChats", User),{
+                    [combineId + ".userInfo"]: {
+                        uid: User,
+                        displayName: userFullname
+                      },
+                      [combineId + ".date"]: serverTimestamp(),
+                    });
+
+                await updateDoc(doc(db,"userChats", messageUserId),{
+                    [combineId+".userInfo"]: {
+                        uid:messageUserId,
+                        displayName: messageUser.fullName
+                    },
+                    [combineId+'.date']: serverTimestamp()
                 });
-            }catch(err){
-                setErr(true)
             }
+        } catch(err) { }
+       
+        setMessageUser(null)
+        setMessageUserId(null)
+        setFullname('')
 
-            
     }
-
-    const handleKey = (e) =>{
-        e.code === "Enter" && handleSearch()
-    }
-    placeholder='Find a user' onKeyDown={handleKey} onChange={e=>setFullname(e.target.value)}
-    */
 
     return(
         <div className='search'>
@@ -67,7 +84,7 @@ const Search = () => {
                 <input className='searchInput' type='text' placeholder='Find a user' onKeyDown={handleKey} onChange={(e)=>setFullname(e.target.value)} value={fullname} />
             </div>
             {err && <span>User not found</span>}
-            {messageUser && <div className='userChat'>
+            {messageUser && <div className='userChat' onClick={handleSelect}>
                 <div className='userChatInfo'>
                     <span>{messageUser.fullName}</span>
                 </div>
