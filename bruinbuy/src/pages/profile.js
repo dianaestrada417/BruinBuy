3b94830de9e12e5206bf2effc5001641c02e4100
Emanuel-Zavalza <emanuelzavalza@gmail.com>
2023-03-13 00:06:36 -0700

    Added tags to item adder in profile

--- bruinbuy/src/pages/profile.js
+++ bruinbuy/src/pages/profile.js
@@ -1,28 +1,27 @@
-import { db } from '../firebase-config';
-import { collection, deleteDoc, doc, addDoc, getDocs, query, where} from 'firebase/firestore';
+import { db, storage } from '../firebase-config';
+import { collection, collectionGroup, deleteDoc, doc, addDoc, getDocs, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import {connectStorageEmulator, getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import {storage} from "../firebase-config";
-import {v4} from "uuid";
+import { connectStorageEmulator, getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
 import { UserContext } from '../contexts/UserContext';
+import defaultPic from "./default-placeholder.png";
+import "./profile.scss";
 
 const Profile = () => {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
 
   const [newItemName, setNewItemName] = useState("");
   const [newItemDesc, setNewItemDesc] = useState("");
   const [newItemPrice, setNewItemPrice] = useState(0);
-  const [newItemQuantity, setNewItemQuantity] = useState(0);
+  const [newItemQuantity, setNewItemQuantity] = useState(-1);
 
   const [urls, setUrls] = useState([]);
   const [itemImageUrls, setImageUrls] = useState("");
 
   const { User, setUser } = React.useContext(UserContext);
-  
+
   const uploadImages = async (event) => {
     const files = event.target.files;
-    
+
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
       const storageRef = ref(storage, `images/${file.name + v4()}`);
@@ -31,73 +30,27 @@ const Profile = () => {
       const url = await getDownloadURL(snapshot.ref);
       console.log(url);
       setUrls(prevUrls => [...prevUrls, url]);
-      
+
     }
   }
 
   const [users, setUsers] = useState([]);
   const [items, setItems] = useState([]);
-  //const usersCollectionRef = collection(db, "signups");
   const itemsCollectionRef = collection(db, "allItems");
   const imagesCollectionRef = collection(db, "images");
-  //we need to be able to reference the user id here
-  //const userID = "HA520x5tGinMehQNelgP";
-  //const usersItemCollectionRef = collection(db, "users", userID, "items");
 
-  
-  /*
-  const createUser = async () => {
-    const document = await addDoc(usersCollectionRef, { 
-      name: newName, 
-      age: Number(newAge), 
-      isVendor: false
-    });
 
-    const newCollectionRef = collection(db, "users", document.id, "items");
-    await addDoc(newCollectionRef, {
-      data: 'user has no items'
-    });
-  }
-
-  
-      <input 
-        placeholder="Name..." 
-        onChange={(event)=> {
-          setNewName(event.target.value)
-        }} 
-      />
-      <input 
-      type="number" 
-      placeholder="Age..." 
-      onChange={(event) => {
-        setNewAge(event.target.value)
-      }} 
-      />
-      
-      <button onClick={createUser}> Create User </button>
-      {users.map((user) => {
-        return (
-          <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <hi>Age: {user.age}</hi>
-          </div>
-        );
-      })}
-
-  const joinURLString = () => {
-    const urlsString = urls.join(',');
-    console.log(urlsString);
-  }*/
-
+  //Create an Item --------------------------------------------------------------------------------------------------------------------------------------------
   const createItem = async () => {
-    //console.log("urls:", urls);
+    console.log("urls:", urls);
     console.log(User);
-    const docRef = await addDoc(itemsCollectionRef, {itemName: newItemName, itemDesc: newItemDesc, itemPrice: Number(newItemPrice), itemQuantity: Number(newItemQuantity), user: User});
-    
-    for(let i=0; i < urls.length; i++){
-      console.log(urls[i]);
-      await addDoc(imagesCollectionRef, {url: urls[i], item: docRef.id});
+    if (newItemName != "" || newItemDesc != "" || Number(newItemPrice) > 0 || Number(newItemQuantity) > 0 || urls.lentgth > 0) {
+      const docRef = await addDoc(itemsCollectionRef, { itemName: newItemName, itemDesc: newItemDesc, itemPrice: Number(newItemPrice), itemQuantity: Number(newItemQuantity), tags: tags, user: User });
+
+      for (let i = 0; i < urls.length; i++) {
+        console.log(urls[i]);
+        await addDoc(imagesCollectionRef, { url: urls[i], item: docRef.id });
+      }
     }
   }
 
@@ -106,40 +59,64 @@ const Profile = () => {
     await deleteDoc(itemDoc);
   };
 
-  /*
+  //Add input tags --------------------------------------------------------------------------------------------------------------------------------------------
+  const [tags, setTags] = React.useState([]);
+  const listOfTags = "";
+
+  const removeTags = indexToRemove => {
+    setTags(tags.filter((_, index) => index != indexToRemove));
+  };
+  const addTags = event => {
+    if(event.target.value != "") {
+      setTags ([...tags, event.target.value]);
+      event.target.value = "";
+    }
+  };
+
+
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
-    };
-
-    getUsers();
-  }, []);
-  */
-
-  useEffect(() =>{
     const getItems = async () => {
       const q = query(itemsCollectionRef, where("user", "==", User));
       const data = await getDocs(q);
-      setItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+      setItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getItems();
   }, []);
 
-  useEffect(() =>{ 
-    const loggedInUser = localStorage.getItem("user"); 
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
     if (loggedInUser) {
-      const foundUser = loggedInUser; 
-      setUser(foundUser); 
-    }; 
+      const foundUser = loggedInUser;
+      setUser(foundUser);
+    };
   }, []);
+
+  useEffect(() => {
+    items.forEach(async (item) => {
+      const itemImages = query(collectionGroup(db, 'images'), where('item', '==', item.id));
+      const imagesSnapshot = await getDocs(itemImages);
+      const imagesData = imagesSnapshot.docs.map((doc) => doc.data().url);
+      if(item.tags.length > 0){
+        for(const tag in item.tags){
+          listOfTags.append(tag);
+          listOfTags.append(', ');
+        }
+      }
+      setItems(prevItems => prevItems.map(prevItem => {
+        if (prevItem.id == item.id) {
+          return { ...prevItem, images: imagesData.length ? imagesData : [defaultPic] };
+        }
+        return prevItem;
+      }));
+    });
+  }, [items]);
 
 
   return (
     <div>
       <tr>
-        <td height="50"></td>
+        <td height="75"></td>
       </tr>
 
       <h1>Add Items</h1>
@@ -149,12 +126,14 @@ const Profile = () => {
           setNewItemName(event.target.value)
         }}
       />
+
       <input
         placeholder="Item Description..."
         onChange={(event) => {
           setNewItemDesc(event.target.value)
         }}
       />
+
       <input
         type="number"
         placeholder="Item Price..."
@@ -162,6 +141,7 @@ const Profile = () => {
           setNewItemPrice(event.target.value)
         }}
       />
+
       <input
         type="number"
         placeholder="Quantity..."
@@ -169,38 +149,73 @@ const Profile = () => {
           setNewItemQuantity(event.target.value)
         }}
       />
-
-      <input 
+      
+      <div className="tags-input">
+        <ul id="tags">
+          {tags.map((tag, index) => (
+            <li key={index} className="tag">
+              <span className='tag-title'>{tag}</span>
+              <span className='tag-close-icon'
+                onClick={() => removeTags(index)}
+              >
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
+          placeholder="Press enter to add tags"
+          onChange={(event) => {
+            setTags(tags)
+          }}
+        />
+      </div>
+      
+      <input
         type="file" multiple
         onChange={uploadImages}
       />
-
+      <tr>
+        <td height="10"></td>
+      </tr>
       <button onClick={createItem}> Add Item </button>
-      {items.map((item) => {
-        if(!item.data){
-          return (
-            <div>
-              {" "}
-              <h1>Name: {item.itemName}</h1>
-              <h2>Desc: {item.itemDesc}</h2>
-              <h2>Quantity: {item.itemQuantity}</h2>
-              <hi>Price: {item.itemPrice}</hi>
+      <tr>
+        <td height="10"></td>
+      </tr>
 
-              <button
-                onClick={() => {
-                  deleteItem(item.id)
-                }}
-              >
-                Delete Item
-              </button>
-            </div>
-          );
-        }
-        
-      })}
-      
+      <div className="items">
+        {items.map((item) => {
+          if (!item.data) {
+            
+            return (
+              <div className="item">
+
+                <div className='itemImage'>
+                  <p>{item.images?.map((url) => <img src={url} alt="" />)}</p>
+                </div>
+
+                <div className='itemInfo'>
+                  <h1>Name: {item.itemName}</h1>
+                  <p>Desc: {item.itemDesc}</p>
+                  <p>Quantity: {item.itemQuantity}</p>
+                  <p>Price: {item.itemPrice}</p>
+                  <p>Tags: {item.tags}</p>
+                  <button
+                    onClick={() => { deleteItem(item.id) }}>
+                    Delete Item
+                  </button>
+                </div>
+              </div>
+
+            );
+          }
+
+        })}
+      </div>
     </div>
   );
 };
-  
+
 export default Profile;
