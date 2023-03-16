bb8a51f92d51d1a8d5dfc1383cd99e4d7bfd0bdf
Peyton Ables <peytonables1@gmail.com>
2023-03-15 17:48:09 -0700

    Added Nav Menu when screen is minimized
    When the screen is minimized, the menu icon will now open a menu to page links that can be used to navigate between different pages.

--- bruinbuy/src/components/Navbar/index.js
+++ bruinbuy/src/components/Navbar/index.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Nav,
   NavLink,
   Bars,
   NavMenu,
+  NavMenuOpen,
+  NavMenuClosed
 } from './NavbarElements';
 import myLogo from './bruinBuyLogoWhite.png';
+import './index.css'
 
-const Navbar = () => {
+const Navbar = () => {const [navbarOpen, setNavbarOpen] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  console.log(isNavExpanded)
     return (
       <>
-        <Nav className="container-fluid">
-          <Bars></Bars>
-          <img src={myLogo} alt="Logo" height={70} width={60} />
-          <NavMenu>
-            <NavLink to='/home' activeStyle>
-            BruinBuy
-            </NavLink>
-            <NavLink to='/marketplace' activeStyle>
-              MarketPlace
-            </NavLink>
-            <NavLink to='/chat' activeStyle>
-              Chat
-            </NavLink>
-            <NavLink to='/profile' activeStyle>
-              Profile
-            </NavLink>
-            <NavLink to='/login' activeStyle>
-              Login
-            </NavLink>
-            <NavLink to='/signup' activeStyle>
-              Sign Up
-            </NavLink>
-          </NavMenu>
+        <Nav>
+            <Bars className="toggle"
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}></Bars>
+            <img src={myLogo} alt="Logo" height={70} width={60} />
+            
+            <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+              <NavMenu>
+                <NavLink to='/home' activeStyle>
+                BruinBuy
+                </NavLink>
+                <NavLink to='/marketplace' activeStyle>
+                  MarketPlace
+                </NavLink>
+                <NavLink to='/chat' activeStyle>
+                  Chat
+                </NavLink>
+                <NavLink to='/profile' activeStyle>
+                  Profile
+                </NavLink>
+                <NavLink to='/login' activeStyle>
+                  Login
+                </NavLink>
+                <NavLink to='/signup' activeStyle>
+                  Sign Up
+                </NavLink>
+              </NavMenu>
+              
+              </div>
         </Nav>
       </>
     );
--- bruinbuy/src/pages/home.js
+++ bruinbuy/src/pages/home.js
@@ -36,41 +36,6 @@ const Home = () => {
               }}>
                 BruinBuy is a marketplace for Bruins to post anything they might want to sell. From homemade products to books or furniture, BruinBuy can help you with all of your selling and buying needs!
               </p>
-              <p style={{
-                fontSize: 18,
-                color: '#071330',
-                justifyContent: 'center'
-                }}>
-                To look at products that you can buy, head to the MarketPlace Page!
-              </p>
-              <p style={{
-                fontSize: 18,
-                color: '#071330',
-                justifyContent: 'center'
-                }}>
-                  To post a product you want to sell or look at your profile details, head to the Profile Page!
-              </p>
-              <p style={{
-                fontSize: 18,
-                color: '#071330',
-                justifyContent: 'center'
-                }}>
-                  To chat with a vendor about a product or ask questions, head to the Chat Page!
-              </p>
-              <p style={{
-                fontSize: 18,
-                color: '#071330',
-                justifyContent: 'center'
-                }}>
-                  To login to your account, head to the Login Page!
-              </p>
-              <p style={{
-                fontSize: 18,
-                color: '#071330',
-                justifyContent: 'center'
-                }}>
-                  To sign up for BruinBuy and become a new member, head to the Sign Up Page!
-              </p>
           </textBlock>
         </background> 
       </div>
